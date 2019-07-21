0bc8f88305bcac8c5d1759110e9ad88697782e4b
rhammer35 <rhammer35@icloud.com>
2019-07-21 17:00:23 -0400

    Edited code to properly converted Date data and subset dataset to required dates

--- plot1.R
+++ plot1.R
@@ -1,8 +1,9 @@
 ## Reads data into data frame from intial txt file in working directory
+power_data <- read.csv2("household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", colClasses = "character")
 
-power_data <- read.csv2("household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
+## Converts data in column to date class
+power_data$Date <- as.Date(power_data$Date, "%d/%m/%Y")
 
-## Converts data in Date and Time columns to single column with date and time class
-power_data$Date <- as.Date(power_data$Date)
-power_data$Time <- strptime(power_data$Time, "%H:%M:%S")
+## Subsets data into desired dataset
+power_data1 <- subset(power_data, Date == '2007-02-01' | Date == '2007-02-02')
 
