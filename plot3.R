4c8f07c2d82e69c7ae81bf2200c4eabb57a23c53
rhammer35 <rhammer35@icloud.com>
2019-07-22 21:19:14 -0400

    Added code to create legend on graph and closed plotting device.

--- plot3.R
+++ plot3.R
@@ -16,3 +16,15 @@ power_data1$Date_Time <- strptime(power_data1$Date_Time, format = "%d/%m/%Y %H:%
 ## Initialize png plotting device
 png(filename = "plot3.png")
 
+## Create line graph with given characteristics of sub_metering_1 to match example in assignment
+with(power_data1, plot(Date_Time, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = ""))
+
+## Add sub_metering_2 and sub_metering_3 to existing graph
+lines(power_data1$Date_Time, power_data1$Sub_metering_2, col = "red")
+lines(power_data1$Date_Time, power_data1$Sub_metering_3, col = "blue")
+
+## Add legend to graph
+legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## End plotting device
+dev.off()
