705a470ed7e41496179f3ab977070e08545a4e35
rhammer35 <rhammer35@icloud.com>
2019-07-22 21:42:08 -0400

    Added code to create all four plots on one plot page

--- plot4.R
+++ plot4.R
@@ -15,3 +15,24 @@ power_data1$Date_Time <- strptime(power_data1$Date_Time, format = "%d/%m/%Y %H:%
 
 ## Initialize png plotting device
 png(filename = "plot4.png")
+
+## Change mfcol parameter to create 4 plots in 1 in 2x2 format
+par(mfcol = c(2, 2))
+
+## Add first plot
+plot(power_data1$Date_Time, power_data1$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
+
+## Add second plot
+with(power_data1, plot(Date_Time, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = ""))
+lines(power_data1$Date_Time, power_data1$Sub_metering_2, col = "red")
+lines(power_data1$Date_Time, power_data1$Sub_metering_3, col = "blue")
+legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Add third plot
+plot(power_data1$Date_Time, power_data1$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
+
+## Add fourth plot
+plot(power_data1$Date_Time, power_data1$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
+
+## End plotting device
+dev.off()
